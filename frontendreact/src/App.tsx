32357add8798b41f747229c44dbea6b9a764cc9f
lucasfbb <lucasfirmino2612@gmail.com>
2025-05-13 11:03:56 -0300

    delete: deleting trash

--- frontendreact/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { useRoutes } from "react-router-dom"; // ✅
-import routes from "./routes";
-
-function App() {
-	const element = useRoutes(routes);
-	return <>{element}</>;
-}
-
-export default App;
